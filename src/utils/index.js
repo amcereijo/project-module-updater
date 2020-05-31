13ae00c0850092eb8fb2a30b53871ed4240d3627
amcereijo <amcereijo@gmail.com>
2020-05-31 12:46:13 +0200

    Improve messages

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,34 @@
+const kleur = require('kleur');
+
+exports.printStart = function printStart(data) {
+  console.log(kleur.green('\nRun process with:'), data, '\n');
+};
+
+function printEmpty(list) {
+  return !list.length ? '[]' : '';
+}
+
+function printErrors(errors) {
+  console.log('Errors:');
+  Object.keys(errors).forEach((block) => {
+    console.log(`  ${block}: ${printEmpty(errors[block])}`);
+    errors[block].forEach((project) => console.log(`    * ${kleur.red(project)}`));
+  });
+}
+
+function printSuccess(success) {
+  console.log(`Sucess: ${printEmpty(success)}`);
+  success.forEach((project) => console.log(`  * ${kleur.green(project.name)}`));
+}
+
 exports.printEnd = function printEnd(data) {
-  const success = data.success.map((el) => el.name);
-  console.log('Process finished with:\n', JSON.stringify({ ...data, success }, null, 2));
+  console.log(kleur.green('\nProcess finished with:'));
+  printErrors(data.errors);
+  printSuccess(data.success);
 };
 
 exports.printProjectsToUpdate = function printProjectsToUpdate(data) {
-  console.log(`Projects to update (${data.length}):`);
+  console.log(kleur.green(`\nProjects to update (${data.length}):`));
   data.forEach((el) => console.log(`* ${el.name}`));
   console.log('\n');
 };
