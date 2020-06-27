c0ef5ee6d4c61f915eb3f0ecc2cfa0f21e46cb00
amcereijo <amcereijo@gmail.com>
2020-06-27 19:09:55 +0200

    Mix tow steps in a single one

--- src/actions/commands.ts
+++ src/actions/commands.ts
@@ -31,21 +31,11 @@ export function buildNpmInstall(opts: CommandBuilder): Command {
   };
 }
 
-export function buildNewBranch(opts: CommandBuilder): Command {
-  return {
-    command: {
-      program: 'git',
-      args: ['branch', buildBranchName(opts.moduleName, opts.branchName)],
-    },
-    cwd: opts.cwd,
-  };
-}
-
 export function buildCheckoutBranch(opts: CommandBuilder): Command {
   return {
     command: {
       program: 'git',
-      args: ['checkout', buildBranchName(opts.moduleName, opts.branchName)],
+      args: ['checkout', '-b', buildBranchName(opts.moduleName, opts.branchName)],
     },
     cwd: opts.cwd,
   };
