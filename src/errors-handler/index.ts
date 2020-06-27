2665d0433267c5b0dfb61c5c5b6658210d35909a
amcereijo <amcereijo@gmail.com>
2020-06-27 18:54:11 +0200

    Fix error adding elemnts to "errors"

--- src/errors-handler/index.ts
+++ src/errors-handler/index.ts
@@ -1,53 +1,23 @@
 import Promise from 'bluebird';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Data, { ErrorHanlder } from '../data';
-=======
-import Data from '../data';
->>>>>>> Change to ts
-=======
-import Data, { ErrorHanlder } from '../data';
->>>>>>> Linting
 
 /**
  *
  * @returns { removeErrors(errorName: String), getErrors() }
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
 export default function buildErrorsHandler(): ErrorHanlder {
-=======
-export default function buildErrorsHandler() {
->>>>>>> Change to ts
-=======
-export default function buildErrorsHandler(): ErrorHanlder {
->>>>>>> Linting
   const errors = new Map();
 
   function removeErrors(name: string) {
     return (list: [Data]) : Promise<[Data]> => {
-<<<<<<< HEAD
       const deleted = list.filter((el) => !el.continue).map((el) => el.name);
       errors.set(name, deleted);
-=======
-      Object.assign(errors, {
-        [name]: list.filter((el) => !el.continue).map((el) => el.name),
-      });
->>>>>>> Change to ts
 
       return <Promise<[Data]>>Promise.filter(list, (el) => el.continue);
     };
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   function getErrors(): Map<string,[string]> {
-=======
-  function getErrors(): Object {
->>>>>>> Change to ts
-=======
-  function getErrors(): Map<string,[string]> {
->>>>>>> Linting
     return errors;
   }
 
